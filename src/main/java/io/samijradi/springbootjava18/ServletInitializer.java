7bf24c04ad43f38df7124846b1f76283f36f0f8d
Sami Jradi <samijradi@gmail.com>
2022-06-12 02:54:15 +0200

    First commit

---
+++ src/main/java/io/samijradi/springbootjava18/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.samijradi.springbootjava18;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootJava18Application.class);
+	}
+
+}
---
+++ src/main/java/io/samijradi/springbootjava18/SpringBootJava18Application.java
@@ -0,0 +1,13 @@
+package io.samijradi.springbootjava18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava18Application.class, args);
+	}
+
+}
